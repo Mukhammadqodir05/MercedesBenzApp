17ab94e7cc9db62d86df532c367001f1fb98ca75
Muhammadqodir <muhammadqodirummatov31@gmail.com>
2024-01-08 14:42:57 +0200

    first commit

--- src/componets/suv.jsx
+++ src/componets/suv.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import blackBg from '/src/CarBackgroundDetails/C_classSedan/SedanImages/blackBg.png'
+import bgRoad1 from '/src/CarBackgroundDetails/C_classSedan/SedanImages/bgRoad1.jpg'
 import SedanBlackBg from '/src/CarBackgroundDetails/C_classSedan/SedanImages/SedanBlackBg.png'
 
 const Suv = () => {
   return (
-    <main className='suvBg  flex w-full h-screen justify-center items-center overflow-hidden'>
+    <main className='flex w-full h-screen justify-center items-center overflow-hidden'
+          style={{backgroundImage: `url(${bgRoad1})`,
+             backgroundPosition: 'center',
+             backgroundSize:'cover',
+             backgroundRepeat: 'no-repeat'}}
+    >
     
     <div className='lg:hidden'  >
          <img src={blackBg} alt="" />
